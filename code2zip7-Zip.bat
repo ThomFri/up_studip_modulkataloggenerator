00371a1d018c5d69901613f2b78deeba4bfbd019
Thom.Fri <thom.fri@me.com>
2019-10-29 15:58:49 +0100

    Added possibility to create zip with 7-zip

---
+++ code2zip7-Zip.bat
@@ -0,0 +1,14 @@
+@echo off
+
+SET currentDir=%~dp0
+SET sevenZIPDir=C:\Program Files (x86)\_Standard\7-Zip\
+
+echo loeschen..
+del "%currentDir%_ZIP\Modulkataloggenerator.zip"
+echo geloescht!
+echo.
+
+echo neu packen...
+"%sevenZIPDir%7z.exe" a "%currentDir%_ZIP\Modulkataloggenerator.zip" "%currentDir%Code\*"
+echo neu gepackt und fertig!
+pause
